73d6f39e745c40f913c3960790253660649e31f6
CassandraYe1 <144005704+CassandraYe1@users.noreply.github.com>
2025-04-21 17:33:33 +0800

    Update model.py

--- diffusion-2T/model.py
+++ diffusion-2T/model.py
@@ -4,7 +4,13 @@ import torch.nn as nn
 
 class DeepNN(nn.Module):
     """
-    Set the neural network model
+    Deep Neural Network model definition.
+    
+    Architecture:
+        Input layer   : 2D   -> 512D + ReLU activation
+        Hidden layer 1: 512D -> 512D + ReLU activation
+        Hidden layer 2: 512D -> 512D + ReLU activation 
+        Output layer  : 512D -> 2D
     """
     
     def __init__(self):
@@ -23,9 +29,21 @@ class DeepNN(nn.Module):
         )
         
     def forward(self, X, Z):
+        """
+        Forward propagation process.
+        
+        Args:
+            X: [N, 2] Input feature tensor
+            Z: [N, 1] Boolean mask tensor used for channel selection
+            
+        Returns:
+            out: [N, 1] Output tensor
+        """
+        
         H = self.InpLayer(X)
         H = self.HiddenLayer1(H)
         H = self.HiddenLayer2(H)
         out = self.OutLayer(H)
         out = out[:,[0]] * Z + out[:,[1]] * (~Z)
         return out
+        
